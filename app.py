c7057de7400563c9d04b3da733e0f4c65231aaa3
ulrich005 <rolandulrich5msoccer@gmail.com>
2025-06-02 21:22:32 -0400

    app.py

--- app.py
+++ app.py
@@ -12,24 +12,9 @@ st.set_page_config(page_title="Crypto Signal App", layout="wide")
 crypto_options = {
     'Bitcoin (BTC)': 'BTC-USD',
     'Ethereum (ETH)': 'ETH-USD',
-    'Tether (USDT)': 'USDT-USD',
-    'BNB': 'BNB-USD',
     'Solana (SOL)': 'SOL-USD',
-    'XRP': 'XRP-USD',
-    'USDC': 'USDC-USD',
     'Cardano (ADA)': 'ADA-USD',
-    'Dogecoin (DOGE)': 'DOGE-USD',
-    'Avalanche (AVAX)': 'AVAX-USD',
-    'Shiba Inu (SHIB)': 'SHIB-USD',
-    'Polkadot (DOT)': 'DOT-USD',
-    'TRON (TRX)': 'TRX-USD',
-    'Toncoin (TON)': 'TON11419-USD',
-    'Chainlink (LINK)': 'LINK-USD',
-    'Polygon (MATIC)': 'MATIC-USD',
-    'Litecoin (LTC)': 'LTC-USD',
-    'Uniswap (UNI)': 'UNI7083-USD',
-    'Internet Computer (ICP)': 'ICP-USD',
-    'Stellar (XLM)': 'XLM-USD'
+    'Dogecoin (DOGE)': 'DOGE-USD'
 }
 
 # Sidebar inputs
